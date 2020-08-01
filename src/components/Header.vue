08968dde6e6687169c69c66e76d423a79b7e9b1a
Angie Spears <angie.spears809@gmail.com>
2020-08-01 13:38:30 -0400

    add assets and create initial components

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="header-container">
+    <h1>Header goes here</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/SingleItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="single-item-container">
+    <h1>Single Item details goes here</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+
+</style>
