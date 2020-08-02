93261a6a13d12d75a075b05b98527f213b2a3d74
Angie Spears <angie.spears809@gmail.com>
2020-08-01 20:43:06 -0400

    style desktop header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,37 @@
 <template>
   <div id="header-container">
-    <h1>Header goes here</h1>
     <div id = "nav">
+      <div class= "links">
         <router-link to="/the-sofa"> The Sofa</router-link>
         <router-link to="/reviews">Reviews</router-link>
         <router-link to ="/about-us"> About Us</router-link>
-        <router-link to= "/">ALL DAY</router-link>
+      </div>
+      <div>
+        <router-link to= "/"><img src="../assets/logo.svg"></router-link>
+      </div>
+      <div class= "links">
         <router-link to= "/showroom">Showroom</router-link>
         <router-link to="/swatches"> Swatches</router-link>
+      </div>
     </div>
     <router-view/>
   </div>
 </template>
 
-<script>
+<style lang="scss" scoped>
+  #nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 44px 20px;
 
-</script>
-
-<style lang="scss">
-
+    .links {
+       a {
+        padding: 0 24px 0 0;
+        text-decoration: none;
+        color: #4B4B4B;
+        font-size: 16px;
+      }
+    }
+  }
 </style>
