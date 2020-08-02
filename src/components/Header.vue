1485c7746081812073cc5ccd1263938f661bfd80
Angie Spears <angie.spears809@gmail.com>
2020-08-02 12:50:55 -0400

    create separate style files for header and single items components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,27 +17,5 @@
     <router-view/>
   </div>
 </template>
+<style lang="scss" scoped src = './style/header.scss'/>
 
-<style lang="scss" scoped>
-  #nav {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-    margin: 35px;
-
-    .links {
-       a {
-        margin: 0 24px 0 0;
-        text-decoration: none;
-        color: #4B4B4B;
-        font-size: 16px;
-
-        &.router-link-exact-active, &:hover {
-          border-bottom: 3px solid #0D6EF0;
-          border-radius: 2px;
-        }
-      }
-    }
-  }
-</style>
