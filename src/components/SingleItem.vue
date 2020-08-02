62e4296fb86a15096708c1111e2ea270d915c121
Angie Spears <angie.spears809@gmail.com>
2020-08-01 22:42:09 -0400

    add initial text

--- src/components/SingleItem.vue
+++ src/components/SingleItem.vue
@@ -1,6 +1,16 @@
 <template>
-  <div id="single-item-container">
-    <h1>Single Item details goes here</h1>
+  <div id='single-product-container'>
+    <div id = 'product-view-container'>
+      <div id = 'details-container'>
+        <div class = 'details first'>
+          <img src = '../assets/padlock.svg' alt = 'padlock'>
+          <p>Secure checkout</p>
+        </div>
+        <div class = 'details'>
+          <p>Ships in 5-7 Business Days</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,6 +18,26 @@
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Raleway&display=swap');
+  #single-product-container {
+    #product-view-container {
+      #details-container {
+        display: flex;
+        justify-content: space-between;
 
+        .details {
+          &.first {
+            display: flex;
+          }
+          p {
+            color: #4B4B4B;
+            font-size: 17px;
+            font-family: 'Raleway', sans-serif;
+            margin: 0 5px;
+          }
+        }
+      }
+    }
+  }
 </style>
