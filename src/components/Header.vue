531fcff4545647cd340be6c726b3276a4644e066
Angie Spears <angie.spears809@gmail.com>
2020-08-03 22:44:03 -0400

    Add desktop view

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,17 +8,23 @@
         <div></div>
       </div>
       <div>
-        <router-link to= "/"><img src="../assets/logo.svg"></router-link>
+        <router-link id = 'mobile-logo' to= "/"><img src="../assets/logo.svg"></router-link>
       </div>
       <div id = 'nav-links-overlay'>
          <a href="#" id = 'close-btn' @click="closeMenu">&times;</a>
         <div id = 'nav-links' @click="closeMenu">
-
-          <router-link to="/the-sofa"> The Sofa</router-link>
-          <router-link to="/reviews">Reviews</router-link>
-          <router-link to ="/about-us"> About Us</router-link>
-          <router-link to= "/showroom">Showroom</router-link>
-          <router-link to="/swatches"> Swatches</router-link>
+          <div class = 'links'>
+            <router-link to="/the-sofa"> The Sofa</router-link>
+            <router-link to="/reviews">Reviews</router-link>
+            <router-link to ="/about-us"> About Us</router-link>
+          </div>
+           <div id = 'logo'>
+            <router-link to= "/"><img src="../assets/logo.svg"></router-link>
+          </div>
+          <div class = 'links'>
+            <router-link to= "/showroom">Showroom</router-link>
+            <router-link to="/swatches"> Swatches</router-link>
+          </div>
         </div>
       </div>
     </div>
@@ -31,15 +37,20 @@ export default {
     openMenu() {
       const links = document.getElementById('nav-links')
       const overlay = document.getElementById('nav-links-overlay')
-      overlay.style.width = '100%'
-      links.style.display = 'flex'
+      if (window.innerWidth < 1024) {
+        overlay.style.width = '100%'
+        links.style.display = 'flex'
+      }
+
 
     },
     closeMenu() {
       const links = document.getElementById('nav-links')
       const overlay = document.getElementById('nav-links-overlay')
-      links.style.display = 'none'
-      overlay.style.width = '0%'
+      if (window.innerWidth < 1024) {
+        links.style.display = 'none'
+        overlay.style.width = '0%'
+      }
     }
   }
 }
